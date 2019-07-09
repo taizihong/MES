73ad7a95b1c263b2d1e2e308b2801aae333d12e9
Administrator <Administrator@192.168.85.197>
2019-07-09 13:26:54 +0800

    MES

--- src/main/java/com/haoyu/controller/AdminController.java
+++ src/main/java/com/haoyu/controller/AdminController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/sys/admin")
 public class AdminController {
 
     @RequestMapping("index.page")
--- src/main/java/com/haoyu/controller/UserController.java
+++ src/main/java/com/haoyu/controller/UserController.java
@@ -18,52 +18,53 @@ import com.haoyu.util.MD5Util;
 @Controller
 public class UserController {
 
-    @Resource
-    private SysUserService sysUserService;
+	@Resource
+	private SysUserService sysUserService;
 
-    @RequestMapping("/logout.page")
-    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        request.getSession().invalidate();
-        String path = "signin.jsp";
-        response.sendRedirect(path);
-    }
+	@RequestMapping("/logout.page")
+	public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		request.getSession().invalidate();
+		String path = "signin.jsp";
+		response.sendRedirect(path);
+	}
 
-    @RequestMapping("/login.page")
-    public void login(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+	@RequestMapping("/login.page")
+	public void login(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 
-        SysUser sysUser = sysUserService.findByKeyword(username);
-        String errorMsg = "";
-        String ret = request.getParameter("ret");
+		SysUser sysUser = sysUserService.findByKeyword(username);
+		String errorMsg = "";
+		String ret = request.getParameter("ret");
 
-        if (StringUtils.isBlank(username)) {
-            errorMsg = "用户名不可以为空";
-        } else if (StringUtils.isBlank(password)) {
-            errorMsg = "密码不可以为空";
-        } else if (sysUser == null) {
-            errorMsg = "查询不到指定的用户";
-        } else if (!sysUser.getPassword().equals(MD5Util.encrypt(password))) {
-            errorMsg = "用户名或密码错误";
-        } else if (sysUser.getStatus() != 1) {
-            errorMsg = "用户已被冻结，请联系管理员";
-        } else {
-            // login success
-            request.getSession().setAttribute("user", sysUser);
-            if (StringUtils.isNotBlank(ret)) {
-                response.sendRedirect(ret);
-            } else {
-                response.sendRedirect("/admin/index.page"); //TODO
-                return;
-            }
-        }
+		if (StringUtils.isBlank(username)) {
+			errorMsg = "用户名不可以为空";
+		} else if (StringUtils.isBlank(password)) {
+			errorMsg = "密码不可以为空";
+		} else if (sysUser == null) {
+			errorMsg = "查询不到指定的用户";
+		} else if (!sysUser.getPassword().equals(MD5Util.encrypt(password))) {
+			errorMsg = "用户名或密码错误";
+		} else if (sysUser.getStatus() != 1) {
+			errorMsg = "用户已被冻结，请联系管理员";
+		} else {
+			// login success
+			request.getSession().setAttribute("user", sysUser);
+			if (StringUtils.isNotBlank(ret)) {
+				response.sendRedirect(ret);
+			} else {
+				response.sendRedirect("/sys/admin/index.page");
+				// TODO
+				return;
+			}
+		}
 
-        request.setAttribute("error", errorMsg);
-        request.setAttribute("username", username);
-        if (StringUtils.isNotBlank(ret)) {
-            request.setAttribute("ret", ret);
-        }
-        String path = "signin.jsp";
-        request.getRequestDispatcher(path).forward(request, response);
-    }
+		request.setAttribute("error", errorMsg);
+		request.setAttribute("username", username);
+		if (StringUtils.isNotBlank(ret)) {
+			request.setAttribute("ret", ret);
+		}
+		String path = "signin.jsp";
+		request.getRequestDispatcher(path).forward(request, response);
+	}
 }
